d1ab23b4696350126c86699c85b088aed9270fd6
yintaoxu <yintaoxu@tencent.com>
2025-05-13 19:45:56 +0800

    添加项目文件。

---
+++ AnimationLearnerD3D11/AnimationLearnerD3D11.cpp
@@ -0,0 +1,158 @@
+﻿#include <windows.h>
+#include <d3d11.h>
+#pragma comment(lib, "d3d11.lib")
+
+// 全局变量
+HWND g_hWnd = nullptr;
+D3D_FEATURE_LEVEL g_featureLevel = D3D_FEATURE_LEVEL_11_0;
+ID3D11Device* g_pd3dDevice = nullptr;
+ID3D11DeviceContext* g_pImmediateContext = nullptr;
+IDXGISwapChain* g_pSwapChain = nullptr;
+ID3D11RenderTargetView* g_pRenderTargetView = nullptr;
+
+// 窗口过程函数
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    if (message == WM_DESTROY)
+    {
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+// 初始化窗口
+HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow)
+{
+    WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0, 0,
+                      GetModuleHandle(NULL), nullptr, nullptr, nullptr, nullptr,
+                      L"DX11SampleClass", nullptr };
+    RegisterClassEx(&wc);
+
+    g_hWnd = CreateWindow(wc.lpszClassName, L"DirectX 11 简单示例",
+        WS_OVERLAPPEDWINDOW, 100, 100, 800, 600,
+        nullptr, nullptr, wc.hInstance, nullptr);
+
+    if (!g_hWnd)
+        return E_FAIL;
+
+    ShowWindow(g_hWnd, nCmdShow);
+    UpdateWindow(g_hWnd);
+
+    return S_OK;
+}
+
+// 初始化 DirectX 11
+HRESULT InitD3D()
+{
+    RECT rc;
+    GetClientRect(g_hWnd, &rc);
+    UINT width = rc.right - rc.left;
+    UINT height = rc.bottom - rc.top;
+
+    DXGI_SWAP_CHAIN_DESC sd = {};
+    sd.BufferCount = 1;
+    sd.BufferDesc.Width = width;
+    sd.BufferDesc.Height = height;
+    sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    sd.BufferDesc.RefreshRate.Numerator = 60;
+    sd.BufferDesc.RefreshRate.Denominator = 1;
+    sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    sd.OutputWindow = g_hWnd;
+    sd.SampleDesc.Count = 1;
+    sd.SampleDesc.Quality = 0;
+    sd.Windowed = TRUE;
+
+    HRESULT hr = D3D11CreateDeviceAndSwapChain(
+        nullptr,                    // 默认适配器
+        D3D_DRIVER_TYPE_HARDWARE,
+        nullptr,
+        0,
+        nullptr, 0,
+        D3D11_SDK_VERSION,
+        &sd,
+        &g_pSwapChain,
+        &g_pd3dDevice,
+        &g_featureLevel,
+        &g_pImmediateContext);
+
+    if (FAILED(hr))
+        return hr;
+
+    // 创建渲染目标视图
+    ID3D11Texture2D* pBackBuffer = nullptr;
+    hr = g_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&pBackBuffer);
+    if (FAILED(hr))
+        return hr;
+
+    hr = g_pd3dDevice->CreateRenderTargetView(pBackBuffer, nullptr, &g_pRenderTargetView);
+    pBackBuffer->Release();
+    if (FAILED(hr))
+        return hr;
+
+    g_pImmediateContext->OMSetRenderTargets(1, &g_pRenderTargetView, nullptr);
+
+    // 设置视口
+    D3D11_VIEWPORT vp;
+    vp.Width = (FLOAT)width;
+    vp.Height = (FLOAT)height;
+    vp.MinDepth = 0.0f;
+    vp.MaxDepth = 1.0f;
+    vp.TopLeftX = 0;
+    vp.TopLeftY = 0;
+    g_pImmediateContext->RSSetViewports(1, &vp);
+
+    return S_OK;
+}
+
+// 清理资源
+void Cleanup()
+{
+    if (g_pImmediateContext) g_pImmediateContext->ClearState();
+    if (g_pRenderTargetView) g_pRenderTargetView->Release();
+    if (g_pSwapChain) g_pSwapChain->Release();
+    if (g_pImmediateContext) g_pImmediateContext->Release();
+    if (g_pd3dDevice) g_pd3dDevice->Release();
+}
+
+// 主消息循环和渲染
+int Run()
+{
+    MSG msg = { 0 };
+    while (msg.message != WM_QUIT)
+    {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        else
+        {
+            // 清屏颜色
+            float clearColor[4] = { 0.2f, 0.4f, 0.6f, 1.0f };
+            g_pImmediateContext->ClearRenderTargetView(g_pRenderTargetView, clearColor);
+
+            // 交换前后缓冲区
+            g_pSwapChain->Present(1, 0);
+        }
+    }
+    return (int)msg.wParam;
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
+{
+    if (FAILED(InitWindow(hInstance, nCmdShow)))
+        return 0;
+
+    if (FAILED(InitD3D()))
+    {
+        Cleanup();
+        return 0;
+    }
+
+    int ret = Run();
+
+    Cleanup();
+
+    return ret;
+}
